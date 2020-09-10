e37378165599896e8ef785d2cdc093ac47416f62
Karina Algarra <kaalgarra04@gmail.com>
2020-09-10 00:10:06 -0500

    MOD SRC carouselitem

--- Src/Components/CarouselItem.jsx
+++ Src/Components/CarouselItem.jsx
@@ -5,28 +5,19 @@ import icon from '../assets/static/playIcon.png';
 import icon2 from '../assets/static/plusIcon.png';
 import '../assets/styles/components/CarouselItem.scss';
 
-const CarouselItem = () => {
+const CarouselItem = () => (
 
   <div className="carousel-item">
     <img className="carousel-item__img" src={img} alt="no estoy" />
     <div className="carousel-item__details">
       <div>
-        <img
-            className="carousel-item__details--img"
-            src={icon}
-            alt="Play Icon"
-            />
-        <img
-            className="carousel-item__details--img"
-            src={icon2}
-            alt="Plus Icon"
-            />
+        <img className="carousel-item__details--img" src={icon} alt="Play Icon" />
+        <img className="carousel-item__details--img" src={icon2} alt="Plus Icon" />
       </div>
-            <p className="carousel-item__details--title">Título descriptivo</p>
-            <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
+      <p className="carousel-item__details--title">Título descriptivo</p>
+      <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
     </div>
   </div>
-  
 );
-}
+
 export default CarouselItem;
